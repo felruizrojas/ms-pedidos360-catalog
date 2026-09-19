d95519225e19e64b37302c66e7a74f35f028b2e6
felruizrojas <fe.ruizr@duocuc.cl>
2026-09-19 00:41:48 -0300

    feat: revisar y editar endpoint post

--- src/main/java/cl/duoc/pedidos360/catalog/dto/ProductoRequest.java
+++ src/main/java/cl/duoc/pedidos360/catalog/dto/ProductoRequest.java
@@ -3,8 +3,10 @@ package cl.duoc.pedidos360.catalog.dto;
 import java.math.BigDecimal;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,13 +19,16 @@ import lombok.NoArgsConstructor;
 public class ProductoRequest {
 
     @NotBlank
+    @Size(max = 255)
     private String nombre;
 
     private String descripcion;
 
+    @NotNull
     @Positive
     private BigDecimal precio;
 
+    @NotNull
     @PositiveOrZero
     private Integer stock;
 
